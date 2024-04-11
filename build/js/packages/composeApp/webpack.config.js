31ff3e2a2ce092a9d688d115266fb7b9465f9b9a
Caner Türe <canerture@teknasyon.com>
2024-04-11 05:19:21 +0300

    add all files

---
+++ build/js/packages/composeApp/webpack.config.js
@@ -0,0 +1,74 @@
+let config = {
+  mode: 'production',
+  resolve: {
+    modules: [
+      "node_modules"
+    ]
+  },
+  plugins: [],
+  module: {
+    rules: []
+  }
+};
+
+// entry
+config.entry = {
+    main: [require('path').resolve(__dirname, "kotlin/composeApp.mjs")]
+};
+config.output = {
+    filename: (chunkData) => {
+        return chunkData.chunk.name === 'main'
+            ? "composeApp.js"
+            : "composeApp-[name].js";
+    },
+    library: "composeApp",
+    libraryTarget: "umd",
+    globalObject: "this"
+};
+config.output.path = require('path').resolve(__dirname, "../../../../composeApp/build/dist/wasmJs/productionExecutable")
+    // source maps
+    config.module.rules.push({
+            test: /\.js$/,
+            use: ["source-map-loader"],
+            enforce: "pre"
+    });
+    config.devtool = 'source-map';
+config.ignoreWarnings = [/Failed to parse source map/]
+    
+// dev server
+config.devServer = {
+  "open": true,
+  "static": [
+    "/Users/canerture/Documents/GitHub/AndroidHub/composeApp"
+  ]
+};
+
+// Report progress to console
+// noinspection JSUnnecessarySemicolon
+;(function(config) {
+    const webpack = require('webpack');
+    const handler = (percentage, message, ...args) => {
+        const p = percentage * 100;
+        let msg = `${Math.trunc(p / 10)}${Math.trunc(p % 10)}% ${message} ${args.join(' ')}`;
+        msg = msg.replace(require('path').resolve(__dirname, "../.."), '');;
+        console.log(msg);
+    };
+
+    config.plugins.push(new webpack.ProgressPlugin(handler))
+})(config);
+
+// noinspection JSUnnecessarySemicolon
+;(function(config) {
+    const tcErrorPlugin = require('kotlin-test-js-runner/tc-log-error-webpack');
+    config.plugins.push(new tcErrorPlugin())
+    config.stats = config.stats || {}
+    Object.assign(config.stats, config.stats, {
+        warnings: false,
+        errors: false
+    })
+})(config);
+config.experiments = {
+    asyncWebAssembly: true,
+    topLevelAwait: true,
+}
+module.exports = config
